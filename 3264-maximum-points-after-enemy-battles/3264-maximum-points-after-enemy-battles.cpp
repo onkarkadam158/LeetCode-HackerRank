6243b6d305fd9775d5fdd4d2d006629b5348773b
OMKAR KADAM <42216838+onkarkadam158@users.noreply.github.com>
2024-07-06 23:07:44 +0530

    Time: 128 ms (16.67%), Space: 75.9 MB (75%) - LeetHub

--- 3264-maximum-points-after-enemy-battles/3264-maximum-points-after-enemy-battles.cpp
+++ 3264-maximum-points-after-enemy-battles/3264-maximum-points-after-enemy-battles.cpp
@@ -7,32 +7,8 @@ public:
         sort(arr.begin(),arr.end());
         if(curr<arr[0]) return 0;
         for(int i=1;i<n;i++){
-            sum+=arr[i]-arr[0];
+            sum+=arr[i];
         }
-        ans+=n-1+((sum+curr)/(arr[0]));
-        return ans;
-
-        for(auto a:arr){
-            if(curr>=a){
-                curr-=a;
-                ans++;
-            }
-            sum+=a;
-            pref.push_back(sum);
-        }
-        
-
-        if(ans==0) return 0;
-        for(int i=n-1;i>=0;i--){
-           curr+=arr[i];
-           int ind=upper_bound(pref.begin(),pref.end(),curr)-pref.begin();
-           curr-=pref[ind-1];
-           cout<<ind<<" ";
-           ind=min(i-1,ind-1);
-           ans+=ind;
-        }
-        return ans;
-
-        
+        return (sum+curr)/arr[0];        
     }
 };
