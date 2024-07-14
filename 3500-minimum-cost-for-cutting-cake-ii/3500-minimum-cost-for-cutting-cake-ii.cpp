45cd8e56f6bd756058bdbc2167ec489e69c4b3d8
OMKAR KADAM <42216838+onkarkadam158@users.noreply.github.com>
2024-07-14 18:08:45 +0530

    Time: 500 ms (55.56%), Space: 234.6 MB (11.11%) - LeetHub

---
+++ 3500-minimum-cost-for-cutting-cake-ii/3500-minimum-cost-for-cutting-cake-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    long long minimumCost(int m, int n, vector<int>& horizontalCut, vector<int>& verticalCut) {
+        long long total=0;
+        sort(horizontalCut.rbegin(), horizontalCut.rend());
+        sort(verticalCut.rbegin(), verticalCut.rend());
+        long long h=0,v=0,hpiece=1,vpiece=1 ; 
+        while(h<m-1 or v<n-1){
+            if(h<m-1 and v<n-1){
+                if(horizontalCut[h] > verticalCut[v]){
+                    total+=horizontalCut[h]*vpiece;
+                    h++;
+                    hpiece++;
+                }
+                else{
+                    total+=verticalCut[v]*hpiece;
+                    v++;
+                    vpiece++;
+                }
+            }
+            else if(h<m-1){
+                total+=horizontalCut[h]*vpiece;
+                h++;
+                hpiece++;
+            }
+            else{
+                total+=verticalCut[v]*hpiece;
+                v++;
+                vpiece++;
+            }
+        }
+        return total;
+    }
+};
