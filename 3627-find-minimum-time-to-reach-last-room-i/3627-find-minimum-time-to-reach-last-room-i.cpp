e5b76b42d92ea7944be780f3cb28d191abdda975
OMKAR KADAM <42216838+onkarkadam158@users.noreply.github.com>
2024-11-29 11:56:18 +0530

    Time: 52 ms (16.08%), Space: 36.7 MB (9.24%) - LeetHub

---
+++ 3627-find-minimum-time-to-reach-last-room-i/3627-find-minimum-time-to-reach-last-room-i.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    #define pii pair<int,pair<int,int>>
+    int m,n;
+    vector<vector<int>>dir={{-1,0},{0,1},{1,0},{0,-1}};
+    bool check(int i,int j){
+        return i>=0 and j>=0 and i<m and j<n;
+    }
+    int minTimeToReach(vector<vector<int>>& grid) {
+        m=grid.size(),n=grid[0].size();
+        priority_queue<pii,vector<pii>,greater<pii>>pq;
+        vector<vector<int>>vis(m,vector<int>(n,0));
+        vis[0][0]=1;
+        pq.push({0,{0,0}});
+
+        while(!pq.empty()){
+            auto top=pq.top();
+            pq.pop();
+            int time=top.first;
+            int x=top.second.first,y=top.second.second;
+            if(x==(m-1) and y==(n-1)) return time;
+            for(auto a:dir){
+                int dx=x+a[0],dy=y+a[1];
+                if(check(dx,dy) and !vis[dx][dy]){
+                    int tempTime=max(1+time,grid[dx][dy]+1);
+                    pq.push({tempTime,{dx,dy}});
+                    vis[dx][dy]=1;
+                }
+            }
+        }
+        return 1e9;
+    }
+};
