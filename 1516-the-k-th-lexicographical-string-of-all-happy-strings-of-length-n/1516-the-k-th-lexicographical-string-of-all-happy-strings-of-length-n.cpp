6e262493b8a49f326aa98c12ba118d7ce7ff2662
OMKAR KADAM <42216838+onkarkadam158@users.noreply.github.com>
2025-02-19 11:39:57 +0530

    Time: 0 ms (100%), Space: 9 MB (77.25%) - LeetHub

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
-    bool solve(int ind,string temp,string& res,int &count,int n,int k){
+    string solve(int ind,string temp,int n,int& k){
         if(ind==n){
-            count++;
-            if(count==k){
-                res=temp;
-                return true;
+            k--;
+            if(k==0){
+                return temp;
             }
-            return false;
+            return "";
         }
         temp+='z';
         for(int i=0;i<3;i++){
-            char c=('a'+i);
-            if(ind==0 or temp[ind-1]!=c){
-                temp[ind]=c;
-                if(solve(ind+1,temp,res,count,n,k)) return true;
+            if(ind==0 or temp[ind-1]!=('a'+i)){
+                temp[ind]=('a'+i);
+                string t=solve(ind+1,temp,n,k);
+                if(t!="") return t;
             }
         }
-        return false;
+        return "";
     }
     string getHappyString(int n, int k) {
-        int count=0;
-        string temp="",res="";
-        solve(0,temp,res,count,n,k);
-        return res;
+        string temp="";
+        return solve(0,temp,n,k);
     }
 };
