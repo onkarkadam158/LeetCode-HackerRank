7c739da384936f0bc988da3079d43ecadf29b630
OMKAR KADAM <42216838+onkarkadam158@users.noreply.github.com>
2025-02-20 10:49:12 +0530

    Time: 0 ms (100%), Space: 13.8 MB (24.71%) - LeetHub

--- 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
@@ -1,29 +1,71 @@
 class Solution {
 public:
-    unordered_map<string,int>mp;
-    string solve(int n, string &temp) {
-        if(temp.size()==n) {
-            if(mp.find(temp)==mp.end()) {
+    int getDecimal(string s){
+        int res=0;
+        for(int i=0;i<s.size();i++){
+            res=2*res + (s[i]-'0');
+        }
+        return res;
+    }
+    string getBinary(int num,int n){
+        string res="";
+        while(n--){
+            res+='0';
+        }
+        int i=0;
+        while(num){
+            if(num%2) res[i]='1';
+            i++;
+            num=num/2;
+        }
+        reverse(res.begin(),res.end());
+        return res;
+    }
+    string solution1(vector<string>&nums){
+        unordered_map<int,int>mp;
+        int n=nums.size();
+        for(auto &a:nums){
+            cout<<getDecimal(a)<<" ";
+            mp[getDecimal(a)]++;
+        }
+        string result="";
+        for(int i=0;i<pow(2,n);i++){
+            if(mp.find(i)==mp.end()){
+                result=getBinary(i,n);
+                break;
+            }
+        }
+        return result;
+    }
+    //SOlution 2
+    string solution2(int n,string& temp,unordered_map<string,int>&mp){
+        if(temp.size()==n){
+            if(mp.find(temp)==mp.end()){
                 return temp;
             }
             return "";
         }
-        temp=temp+"0";
-        string left=solve(n,temp);
-        if(left.size()>0) return left;
+        temp+='0';
+        string res=solution2(n,temp,mp) ;
+        if(res.size()==n) return res;
         temp.pop_back();
 
-        temp=temp+"1";
-        string right = solve(n,temp);
-        if(right.size()>0) return right;
+        temp+='1';
+        res=solution2(n,temp,mp) ;
+        if(res.size()==n) return res;
         temp.pop_back();
 
         return "";
     }
     string findDifferentBinaryString(vector<string>& nums) {
-        for(auto a:nums) mp[a]++;
+        // return solution1(nums); // Without recursion solution
+        
         string temp="";
-        int n=nums[0].size();
-        return solve(n,temp);
+        int n=nums.size();
+        unordered_map<string,int>mp;
+        for(auto str:nums) mp[str]++;
+
+        return solution2(n,temp,mp);
+         
     }
 };
