a47556552d389d7625a3df54f56599ddaf990d43
OMKAR KADAM <42216838+onkarkadam158@users.noreply.github.com>
2025-02-28 20:00:32 +0530

    Time: 25 ms (60.06%), Space: 26.6 MB (87.72%) - LeetHub

---
+++ 1170-shortest-common-supersequence/1170-shortest-common-supersequence.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    void print(vector<vector<int>>&dp){
+        for(auto &a:dp){
+            for(auto &x:a){
+                cout<<x<<" ";
+            }
+            cout<<endl;
+        }
+    }
+    string shortestCommonSupersequence(string s1, string s2) {
+        int n=s1.size(),m=s2.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1[i-1]==s2[j-1]) { dp[i][j]=1+dp[i-1][j-1];}
+                else {dp[i][j]=max(dp[i-1][j],dp[i][j-1]);}
+            }
+        }
+        // print(dp);
+        string result="";
+        int i=n,j=m;
+        while(i>0 and j>0){
+            if(s1[i-1]==s2[j-1]){
+                result+=s1[i-1];
+                i--,j--;
+            }
+            else if(dp[i-1][j]>dp[i][j-1]){
+                result+=s1[i-1];
+                i--;
+            }
+            else{
+                result+=s2[j-1];
+                j--;
+            }
+        }
+        // cout<<result<<" ";
+        while(i>0) result+=s1[i-1],i--;
+        while(j>0) result+=s2[j-1],j--;
+        reverse(result.begin(),result.end());
+        return result;
+    }
+};
