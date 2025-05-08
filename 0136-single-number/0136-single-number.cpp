5bec318c70917dafb8a0da41e9af1402fd7963ff
OMKAR KADAM <42216838+onkarkadam158@users.noreply.github.com>
2025-05-08 22:25:41 +0530

    Time: 0 ms (100%), Space: 20.6 MB (83.84%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,34 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int res=0;
-        for(int i=0;i<nums.size();i++)
-            res=res^nums[i];
+        int n=nums.size(),res=0;
+        for(int i=0;i<n;i++){
+            res^=nums[i];
+        }
         return res;
-        /*for(int i=0;i<n;i++)
-        {
-            int temp=nums[i];
-            temp= (temp>0)?temp:-1*temp;
-            nums[temp-1]*=(-1);
-        }
-        int i;
-        for( i=0;i<n;i++)
-        {
-            if(nums[i]<0)
-                break;
-        }
-        return i+1;
-        sort(nums.begin(),nums.end());
-        int res;
-        for(int i=1;i<n;i++)
-            if(nums[i-1]==nums[i])
-            {
-                nums[i-1]=0;
-                nums[i]=0;
-            }
-        for(int i=0;i<n;i++)
-            if(nums[i]!=0)
-                res=nums[i];
-        return res; */       
     }
 };
