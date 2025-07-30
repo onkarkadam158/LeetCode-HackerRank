351472454264e3b7a1a0cfc8bdd7a7efa695282d
OMKAR KADAM <42216838+onkarkadam158@users.noreply.github.com>
2025-07-30 23:22:19 +0530

    Time: 0 ms (100%), Space: 86.2 MB (7.99%) - LeetHub

--- 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.cpp
+++ 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.cpp
@@ -1,46 +1,45 @@
 class Solution {
 public:
-    int bruteforce(vector<int>&nums){
-        int n=nums.size(),result=0,size=0;
+    int bruteforce(vector<int>& nums) {
+        int n=nums.size();
+        int maxAnd=0,result=1;
         for(int i=0;i<n;i++){
             int temp=nums[i];
-            for(int j=i;j<n;j++){
-                temp=(temp&nums[j]);
-                result=max(result,temp);
+            for( int j=i ;j<n;j++){
+                temp=temp&nums[j];
+                maxAnd=max(maxAnd,temp);
             }
         }
-        //result has maxi AND of any subarray
         for(int i=0;i<n;i++){
             int temp=nums[i];
-            for(int j=i;j<n;j++){
-                temp=(temp&nums[j]);
-                if(temp==result){
-                    size=max(size,j-i+1);
+            for(int j=i+1;j<n;j++){
+                temp=temp&nums[j];
+                if(temp==maxAnd){
+                    result=max(result,j-i+1);
                 }
             }
-        }
-        return size;
-    }
-    int optimal(vector<int>&nums){
-        int res=0;
-        for(auto a:nums){
-            res=max(res,a);
-        }
-        int result=1,count=1;
-        for(int i=1;i<nums.size();i++){
-            if(nums[i]==nums[i-1] and nums[i]==res){
-                count++;
-            }
-            else{
-                count=1;
-            }
-            result=max(result,count);
         }
         return result;
     }
     int longestSubarray(vector<int>& nums) {
-        // return bruteforce(nums); //TLE O(n^2) time space O(1)
+        // return bruteforce(nums);
+        int n=nums.size();
+        int maxEle=*max_element(nums.begin(),nums.end());
+        int res=1;
+        for(int i=0;i<n;){
+            if(nums[i]==maxEle){
+                int count=0;
+                while(i<n and nums[i]==maxEle){
+                    i++;
+                    count++;
+                }
+                res=max(res,count);
+            }
+            else{
+                i++;
+            }
+        }
 
-        return optimal(nums);
+        return res;
     }
 };
